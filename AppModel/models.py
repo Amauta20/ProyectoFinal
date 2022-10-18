c59f996d19c0c5dc266d58d6bb762cb62c114852
Amauta20 <86487903+Amauta20@users.noreply.github.com>
2022-10-18 07:24:42 -0500

    Agregamos nacimiento y diseño

--- AppModel/models.py
+++ AppModel/models.py
@@ -6,8 +6,8 @@ class Familiar(models.Model):
     nombres = models.CharField(max_length=100)
     direccion = models.CharField(max_length=200)
     numero_pasaporte = models.IntegerField()
-    nacimiento = models.DateField()
+    nacimiento = models.CharField(max_length=10)
 
 def __str__(self):
-    return f"{self.nombres}, {self.numero_pasaporte}, {self.id}"
+    return f"{self.nombres}, {self.numero_pasaporte}, {self.direccion}, {self.nacimiento}, {self.id}"
 
