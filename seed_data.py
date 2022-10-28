dfcbd15de1339520c5d67267591ddd9ab167039a
Steve Chunga <86487903+Amauta20@users.noreply.github.com>
2022-10-27 23:28:16 -0500

    @Pinguiborja @Valentinaherlein
    Nuevo commit

--- seed_data.py
+++ seed_data.py
@@ -1,12 +1,12 @@
 from AppModel.models import Familiar
 
-Familiar(nombre="Rosario", direccion="Rio Parana 745", numero_pasaporte=123123).save()
-Familiar(nombre="Alberto", direccion="Rio Parana 745", numero_pasaporte=890890).save()
-Familiar(nombre="Samuel", direccion="Rio Parana 745", numero_pasaporte=345345).save()
-Familiar(nombre="Florencia", direccion="Rio Parana 745", numero_pasaporte=567567).save()
-Familiar(nombre="Juan", direccion="Rio Plata 745", numero_pasaporte=101023).save()
-Familiar(nombre="Lucio", direccion="Rio Plata 745", numero_pasaporte=823490).save()
-Familiar(nombre="Anabell", direccion="Rio Plata 745", numero_pasaporte=345266).save()
-Familiar(nombre="Pedro", direccion="Rio Plata 745", numero_pasaporte=567135).save()
+Familiar(nombres="Rosario", direccion="Rio Parana 745", numero_pasaporte=123123, nacimiento="12/10/2008").save()
+Familiar(nombres="Alberto", direccion="Rio Parana 745", numero_pasaporte=890890, nacimiento="11/10/2001").save()
+Familiar(nombres="Samuel", direccion="Rio Parana 745", numero_pasaporte=345345, nacimiento="02/01/1979").save()
+Familiar(nombres="Florencia", direccion="Rio Parana 745", numero_pasaporte=567567, nacimiento="12/10/2001").save()
+Familiar(nombres="Juan", direccion="Rio Plata 745", numero_pasaporte=101023, nacimiento="12/10/2002").save()
+Familiar(nombres="Lucio", direccion="Rio Plata 745", numero_pasaporte=823490, nacimiento="12/10/2003").save()
+Familiar(nombres="Anabell", direccion="Rio Plata 745", numero_pasaporte=345266, nacimiento="12/10/2004").save()
+Familiar(nombres="Pedro", direccion="Rio Plata 745", numero_pasaporte=567135, nacimiento="12/10/2000").save()
 
 print("Se cargo con éxito los usuarios de pruebas")
